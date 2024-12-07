86a7e4cc0abd9ea11deed132300f54bdf9cd7a78
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-12-07 15:28:22 -0800

    Part 1 commit

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string> 
+
 using namespace std;
 
+int sum_ascii(string);
 
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    
+    // char a = 'A';
+    // cout << a << endl;
+    // cout << (int) a << endl;
+    // int b = 66;
+    // cout << b << endl;
+    // cout << (char) b << endl;
+
+    string test1 = "Aa"; 
+
+    int test = sum_ascii(test1);
+
+    cout << "\nTest Value: " << test << endl;
 
     return 0;
 }
@@ -21,3 +29,23 @@ These targets are present in the dataset and can be used for testing:
 666D109AA22E
 E1D2665B21EA
 */
+
+
+int sum_ascii(string s) {
+
+    // create accumulator variable to store total values
+    int ascciSum = 0;
+
+    // loop through the string entered in the function
+    for (char c : s) {
+        // add each chars integer value into the accumulator 
+        ascciSum += (int) c;
+    }
+
+    return ascciSum;
+}
+
+
+
+
+
