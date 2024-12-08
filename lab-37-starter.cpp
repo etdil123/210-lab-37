4b3b3e916980a0e1ecda8ee3cc471ed04d2b1f1b
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-12-07 20:03:11 -0800

    10 min commit

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -70,6 +70,7 @@ int main() {
                 cout << "Please enter key you would like to be removed: " << endl;
                 cin >> removeKey;
 
+                removeHash(hash_table, removeKey);
                 break;
             }
             case 5:
@@ -209,15 +210,17 @@ void removeHash(map<int, list<string>> &hash_table, string removeString) {
 
     // search through hash_table looking for hashIndex - if found loop through list
     if (searchMap != hash_table.end()) {
-        // iterate through list
-        for (auto it = hash_table[hashIndex].begin() ; it != hash_table[hashIndex].end();) {
-            if (*it == removeString)
-                hash_table[hashIndex].erase(it);
+        // Create iterator to move through the list from beginning 
+        for (auto it = hash_table[hashIndex].begin() ; it != hash_table[hashIndex].end(); ) {
+            if (*it == removeString) {
+                it = hash_table[hashIndex].erase(it);
                 cout << removeString << " has been removed from hash table!" << endl;
                 return;
+            }
+            else 
+                ++it;
         }
 
-        
         // If string has not been found print to user that it cannot be removed
         cout << removeString << " cannot be removed - value was not found in hash table!" << endl;
 
